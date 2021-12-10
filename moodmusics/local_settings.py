6ff2bac7eb3768182dea5d556cf02d478203429d
Deepanshu Mishra <49090565+deepanshumishra@users.noreply.github.com>
2021-12-11 00:46:04 +0530

    first push

---
+++ moodmusics/local_settings.py
@@ -0,0 +1,10 @@
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
